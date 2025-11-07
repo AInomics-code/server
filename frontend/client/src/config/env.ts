8174deaaac7de00619a236d3dc08f8a9ce1c3599
Jeff <jeffreysaa@outlook.com>
2025-11-07 09:21:55 -0500

    feat: vite config

--- frontend/client/src/config/env.ts
+++ frontend/client/src/config/env.ts
@@ -1,3 +1,3 @@
 export const ENV_CONFIG = {
-    API_URL: import.meta.env.VITE_API_URL || 'http://18.219.47.1:8000'
+    API_URL: import.meta.env.VITE_API_URL || 'https://ladonaapi.ainomics.online'
 };
